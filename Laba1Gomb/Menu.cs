e4dec2f4708995d017a71a36a518b6b599ee7648
gomselars@gmail.com <gomselars@gmail.com>
2025-10-28 00:44:10 +0700

    изменила в menu

--- Laba1Gomb/Menu.cs
+++ Laba1Gomb/Menu.cs
@@ -108,7 +108,7 @@ namespace Laba1Gomb
                 string? firm = Console.ReadLine();
 
                 Console.Write("Введите цену: ");
-                if (!float.TryParse(Console.ReadLine(), out float price))
+                if (!decimal.TryParse(Console.ReadLine(), out decimal price))
                 {
                     Console.WriteLine("Ошибка: Некорректный формат цены");
                     return;
@@ -154,7 +154,7 @@ namespace Laba1Gomb
                 string? firm = Console.ReadLine();
 
                 Console.Write("Введите цену: ");
-                if (!float.TryParse(Console.ReadLine(), out float price))
+                if (!decimal.TryParse(Console.ReadLine(), out decimal price))
                 {
                     Console.WriteLine("Ошибка: Некорректный формат цены");
                     return;
